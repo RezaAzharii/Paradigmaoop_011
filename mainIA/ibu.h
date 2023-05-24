461b42bd79474011607889cabb2c718eef02edab
RezaAzharii <rezaazhari867@gmail.com>
2023-05-24 18:02:35 +0700

    membuat class ibu didalam header yang akan dipakai dipanggil dalam program utama

--- mainIA/ibu.h
+++ mainIA/ibu.h
@@ -5,6 +5,16 @@ class ibu {
 public:
 	string nama;
 	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "ibu \"" << nama << "\" tidak ada\n";
+	}
+
+	void tambahAnak(anak*);
+	void cetakAnak();
 };
 
 #endif
